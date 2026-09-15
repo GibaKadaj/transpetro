20d7119eee7f770c8e1c6c519fe9b0645a9f0339
GA Nexus <gadesign25@gmail.com>
2026-09-14 21:07:26 -0300

    Delete app directory

--- app/src/main/java/com/prep/transpetro/domain/model/CutoffStatus.kt
+++
@@ -1,8 +0,0 @@
-package com.prep.transpetro.domain.model
-
-sealed class CutoffStatus {
-    object PassesAll : CutoffStatus()
-    object BelowSecurityMinimum : CutoffStatus()
-    object BelowGeneralMinimum : CutoffStatus()
-    data class EliminatedZero(val subject: String) : CutoffStatus()
-}
